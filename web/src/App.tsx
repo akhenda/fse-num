0ac096fe5b23712003decdbf55c66345c7cfd664
akhenda <akhenda@gmail.com>
2025-03-20 12:05:12 +0300

    chore(web): set up tailwind & shadcn

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,6 @@
 import { AddNewPayment } from './components/AddNewPayment';
 import { LoansAndPayments } from './components/LoansAndPayments';
+import { Button } from './components/ui/button';
 
 import './App.css';
 
@@ -11,6 +12,10 @@ function App() {
 
         <h1>Add New Payment</h1>
         <AddNewPayment />
+
+        <div className="flex flex-col items-center justify-center min-h-svh">
+          <Button>Click me</Button>
+        </div>
       </div>
     </>
   );
